abc2296d0a6c900173ac7d25548433e08c8ff78b
AdamJenkinson-Graham <adamtjg@gmail.com>
2016-04-29 15:49:11 +0100

    First Site
    First site project nearly done

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var opts = { duration: 800, delay: 0, distance: '30px', viewFactor: 0.2, reset: true };
+
+window.sr = ScrollReveal(opts);
+sr.reveal('.appear');
+
+
+
+/*Scroll transition to anchor*/
+$("a.toscroll").on('click',function(e) {
+    var url = e.target.href;
+    var hash = url.substring(url.indexOf("#")+1);
+    $('html, body').animate({
+        scrollTop: $('#'+hash).offset().top
+    }, 600);
+    return false;
+});
+
+$('.menu-toggle').click(function(){
+	$('#menu').toggleClass('hidden');	
+})
+
+
+
+
+
